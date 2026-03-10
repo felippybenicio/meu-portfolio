6a2b8c50592590dc4943e071b6aa42ea602bada4
felippybenicio <desenvolvedor.felippybenicio24@gmail.com>
2026-03-10 13:02:51 -0300

    portfolio finalizado

--- assets/js/secao_projetos.js
+++ assets/js/secao_projetos.js
@@ -1,1 +1,28 @@
-const QRcode = 
+const QRcode = document.querySelectorAll(".QRcode")
+const linkQRcode = document.querySelectorAll(".linkQRcode")
+const overlay = document.querySelectorAll(".overlay")
+
+linkQRcode.forEach((link, index) => {
+
+    link.addEventListener("click", () => {
+
+        QRcode[index].classList.remove("hidden")
+        overlay[index].classList.remove("hidden")
+
+    })
+
+})
+
+document.addEventListener("click", (e) => {
+
+    const clicouQR = e.target.closest(".QRcode");
+    const clicouLink = e.target.closest(".linkQRcode");
+
+    if (!clicouQR && !clicouLink) {
+
+        QRcode.forEach(qr => qr.classList.add("hidden"));
+        overlay.forEach(o => o.classList.add("hidden"));
+
+    }
+
+});
